afaba2a5ac465223b7afa2b6edf35c5bb1a46469
David Brunow <dbrunow@nctc.edu>
2017-04-20 20:48:49 -0500

    Add project files.

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            this.linkDescriptionLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "All you need is love";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "I am the walrus";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Strawberry fields forever";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Coo coo ca-choo";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(108, 219);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(55, 13);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "linkLabel1";
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(108, 258);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(55, 13);
+            this.linkLabel2.TabIndex = 5;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "linkLabel2";
+            // 
+            // linkLabel3
+            // 
+            this.linkLabel3.AutoSize = true;
+            this.linkLabel3.Location = new System.Drawing.Point(108, 299);
+            this.linkLabel3.Name = "linkLabel3";
+            this.linkLabel3.Size = new System.Drawing.Size(55, 13);
+            this.linkLabel3.TabIndex = 6;
+            this.linkLabel3.TabStop = true;
+            this.linkLabel3.Text = "linkLabel3";
+            // 
+            // linkDescriptionLabel
+            // 
+            this.linkDescriptionLabel.AutoSize = true;
+            this.linkDescriptionLabel.Location = new System.Drawing.Point(117, 182);
+            this.linkDescriptionLabel.Name = "linkDescriptionLabel";
+            this.linkDescriptionLabel.Size = new System.Drawing.Size(0, 13);
+            this.linkDescriptionLabel.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(284, 456);
+            this.Controls.Add(this.linkDescriptionLabel);
+            this.Controls.Add(this.linkLabel3);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.LinkLabel linkLabel3;
+        private System.Windows.Forms.Label linkDescriptionLabel;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public class LinkObject
+    {
+        public string Link { get; set; }
+        public string Description { get; set; }
+    }
+
+    public partial class Form1 : Form
+    {
+        List<LinkObject> linkList = new List<LinkObject>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            var link1 = new LinkObject();
+            link1.Link = "https://apple.com";
+            link1.Description = "The Apple website";
+
+            var link2 = new LinkObject();
+            link2.Link = "https://techmill.co";
+            link2.Description = "The TechMill website";
+
+            var link3 = new LinkObject();
+            link3.Link = "http://stokedenton.com";
+            link3.Description = "The Stoke Denton website";
+
+            linkList.Add(link1);
+            linkList.Add(link2);
+            linkList.Add(link3);
+            
+            foreach (Control thisControl in this.Controls)
+            {
+                if (thisControl.GetType() == typeof(Label))
+                {
+                    thisControl.MouseEnter += new EventHandler(handleMouseEnter);
+                    thisControl.MouseLeave += new EventHandler(handleMouseLeave);
+                }
+                else if (thisControl.GetType() == typeof(LinkLabel))
+                {
+                    var linklabel = (LinkLabel)thisControl;
+                    linklabel.LinkClicked += new LinkLabelLinkClickedEventHandler(handleLinkClicked);
+                    linklabel.MouseEnter += new EventHandler(handleMouseEnterLinkLabel);
+                    linklabel.MouseLeave += new EventHandler(handleMouseLeaveLinkLabel);
+
+                }
+            }
+
+            setupLinkLabels();
+        }
+
+        private void handleLinkClicked(object sender, EventArgs e)
+        {
+            var linkLabel = (LinkLabel)sender;
+            var url = linkLabel.Text;
+            linkLabel.LinkVisited = true;
+
+            // Navigate to a URL.
+            System.Diagnostics.Process.Start(url);
+
+            LinkObject linkObjectToRemove = new LinkObject();
+
+            foreach (var link in linkList)
+            {
+                if (link.Link == url)
+                {
+                    linkObjectToRemove = link;
+
+                    break;
+                }
+            }
+            linkList.Remove(linkObjectToRemove);
+            linkList.Insert(0, linkObjectToRemove);
+
+            setupLinkLabels();
+        }
+
+        private void handleMouseEnterLinkLabel(object sender, EventArgs e)
+        {
+            var linkLabel = (LinkLabel)sender;
+            var url = linkLabel.Text;
+            LinkObject linkObjectToShow = new LinkObject();
+
+            foreach (var link in linkList)
+            {
+                if (link.Link == url)
+                {
+                    linkObjectToShow = link;
+
+                    break;
+                }
+            }
+            linkDescriptionLabel.Text = linkObjectToShow.Description;
+        }
+
+        private void handleMouseLeaveLinkLabel(object sender, EventArgs e)
+        {
+            linkDescriptionLabel.Text = "";
+        }
+
+        private void setupLinkLabels()
+        {
+            linkLabel1.Text = linkList[0].Link;
+            linkLabel2.Text = linkList[1].Link;
+            linkLabel3.Text = linkList[2].Link;
+        }
+
+        private void handleMouseEnter(object sender, EventArgs e)
+        {
+            ((Label)sender).BackColor = Color.White;
+        }
+
+        private void handleMouseLeave(object sender, EventArgs e)
+        {
+            ((Label)sender).BackColor = Color.Black;
+        }
+    }
+}
